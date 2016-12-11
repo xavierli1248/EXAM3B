b795c9634ecec41b72f6d00cbab26a0445d7b011
Mike Colbert <michael-colbert@uiowa.edu>
2016-12-12 06:36:28 +0800

    Updated query classes

---
+++ src/java/dbHelpers/DeleteQuery.java
@@ -0,0 +1,66 @@
+package dbHelpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author XavierLi1248
+ */
+public class DeleteQuery {
+
+    private Connection conn;
+
+    public DeleteQuery() {
+        Properties props = new Properties();
+        InputStream instr = getClass().getResourceAsStream("dbConn.properties");
+        try {
+            props.load(instr);
+        } catch (IOException ex) {
+            Logger.getLogger(DeleteQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            instr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(DeleteQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        String driver = props.getProperty("driver.name");
+        String url = props.getProperty("server.name");
+        String username = props.getProperty("user.name");
+        String passwd = props.getProperty("user.password");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DeleteQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(url, username, passwd);
+        } catch (SQLException ex) {
+            Logger.getLogger(DeleteQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void doDelete(int custID) {
+
+        try {
+            String query = "DELETE FROM customers WHERE custID = ?";
+
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            ps.setInt(1, custID);
+
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(DeleteQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/java/dbHelpers/SearchQuery.java
@@ -0,0 +1,178 @@
+package dbHelpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Customers;
+
+public class SearchQuery {
+    
+    private Connection conn;
+    private ResultSet results;
+    
+    public SearchQuery(){
+        
+        Properties props = new Properties();
+        InputStream instr = getClass().getResourceAsStream("dbConn.properties");
+        try {
+            props.load(instr);
+        } catch (IOException ex) {
+            Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            instr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        String driver = props.getProperty("driver.name");
+        String url = props.getProperty("server.name");
+        String username = props.getProperty("user.name");
+        String passwd = props.getProperty("user.password");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(url, username, passwd);
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void doSearch(String firstName, String lastName){
+        
+        try {
+            String query = "SELECT * FROM basketballplayer WHERE UPPER(firstName) LIKE ? or UPPER(lastName) LIKE ? ORDER BY playerID ASC";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, "%" + firstName.toUpperCase() + lastName.toUpperCase() + "%");
+            this.results = ps.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public String getHTMLTable(){
+        
+        String table = "";
+        
+        table += "<table>";
+        
+                table += "<th>";
+                table += "CustID";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "FirstName";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "LastName";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "Addr1";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "Addr2";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "City";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "State";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "Zip";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "EmailAddr";
+                table += "</th>";
+                
+                table += "<th>";
+                table += "";
+                table += "</th>";
+        
+        try {
+            while(this.results.next()){
+                
+                Customers customer = new Customers();
+                customer.setCustID(this.results.getInt("custID"));
+                customer.setFirstName(this.results.getString("firstName"));
+                customer.setLastName(this.results.getString("lastName"));
+                customer.setAddr1(this.results.getString("addr1"));
+                customer.setAddr2(this.results.getString("addr2"));
+                customer.setCity(this.results.getString("city"));
+                customer.setState(this.results.getString("state"));
+                customer.setZip(this.results.getString("zip"));
+                customer.setEmailAddr(this.results.getString("emailAddr"));
+                
+                table += "<tr>";
+
+                table += "<td>";
+                table += customer.getCustID();
+                table += "</td>";
+                
+                table += "<td>";
+                table += customer.getFirstName();
+                table += "</td>";
+                
+                table += "<td>";
+                table += customer.getLastName();
+                table += "</td>";
+                
+                table += "<td>";
+                table += customer.getAddr1();
+                table += "</td>";
+                
+                table += "<td>";
+                table += customer.getAddr2();
+                table += "</td>";
+                
+                table += "<td>";
+                table += customer.getCity();
+                table += "</td>";
+                
+                table += "<td>";
+                table += customer.getState();
+                table += "</td>";
+                
+                table += "<td>";
+                table += customer.getZip();
+                table += "</td>";
+                
+                table += "<td>";
+                table += customer.getEmailAddr();
+                table += "</td>";
+                
+                table += "<td>";
+                table += "<a href=update?custID=" + customer.getCustID() + "> Update </a>" + "<a href=delete?custID=" + customer.getCustID() + "> Delete </a>";
+                table += "</td>";
+                
+            table += "</tr>";
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SearchQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        table += "</table>";
+                
+                return table;
+    }
+    
+}
